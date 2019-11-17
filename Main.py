f97fd0e8ddcfab708b444d01c150cd1bc8a539cd
nassorh <nassorh.dev@gmail.com>
2019-11-17 19:08:51 +0000

    fetchExtension function created

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
-Python 2.7 (r27:82525, Jul  4 2010, 07:43:08) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>>
-import os 
+
+import os
+
+downloadDir = "C:\\Users\\Hamad Nassor\\Downloads"
 
 #Database
 audioFiles = ["aif","cda","mid","midi","mp3","mpa","ogg","wav","wma","wpl"]#a,c,m,o,w,w
@@ -18,3 +18,35 @@ spreadsheetFiles=["ods","xlr","xls","xlsx"]#o,x
 systemRelatedFiles=["bak","cab","cfg","cpl","cur","dll","dmp","drv","icns","ico","ini","ink","msi","sys","tmp"]#b,c,d,i,m.s,y
 videoFiles=["3g2","2gp","avi","flv","h264","m4v","mkv","mov","mp4","mpg","mpeg","rm","swf","vob","wmv"]#2,3,a,f,h,m,r,s,v,w
 wordFiles=["doc","docx","odt","pdf","rtf","tex","txt","wks","wpks","wpd"]#d,o,p,r,t,w
+
+def fetchExtension(folderDir):
+    #Define all the extension array
+    extensions = []
+
+    #Status whether to add to the array or not
+    status = True
+
+    #All files in the folders
+    arrayOfFiles = os.listdir(folderDir)
+
+    #Fetches all the extension
+    for file in arrayOfFiles:
+        if "." in file:
+            extension = file.split(".")
+
+            #If it not able to print it doesn't append to the extension list
+            try:
+                print(file,extension[-1])
+                status = True
+            except:
+                pass
+                status = False
+            if status:
+                extensions.append((file,extension[1]))
+
+    #Returns an array of extensions
+    return extensions
+
+
+fileExtension = fetchExtension(downloadDir)
+print(fileExtension)
