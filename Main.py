15eb8046b05968fb261f29bf225630de9fbf8124
nassorh <nassorh.dev@gmail.com>
2019-11-18 18:21:31 +0000

    sortCatergory function created

--- Main.py
+++ Main.py
@@ -1,23 +1,16 @@
-
 import os
 
 downloadDir = "C:\\Users\\Hamad Nassor\\Downloads"
 
-#Database
-audioFiles = ["aif","cda","mid","midi","mp3","mpa","ogg","wav","wma","wpl"]#a,c,m,o,w,w
-compressedFiles = ["7z","arj","deb","pkg","rar","rpm","tar.gz","z","zip"]#7,a,d,p,r,t,z
-discMediaFiles = ["bin","dmg","iso","toast","vcd"]#b,d,i,t,v
-dataDatabaseFiles=["csv","dat","db","dbf","log","mdb","sav","sql","tar","xml"]#c,d,l,m,s,t,x
-executable = ["apk","bat","bin","cgi","pl","com","exe","gadget","jar","py","wsf"]#a,b,c,p,l,c,e,g,j,p,w
-fontFile =["fnt","fon","otf","ttf"]#f,f,o,t
-imageFile = ["ai","bmp","gif","ico","jpeg","jpg","png","ps","psd","svg","tif","tiff"]#a,b,g,i,j,p,s,t
-internetRelatedFiles = ["asp","aspx","cer","cfm","cgi","pl","css","htm","html","js","jsp","part","php","py","rss","xhtml"]#a,c,p,c,h,j,p,r,x
-presentationFiles = ["key","odp","pss","ppt","pptx"]#k,o,p
-programmingFiles = ["c","class","cpp","cs","h","java","sh","swift","vb"]#c,h,j,s,v
-spreadsheetFiles=["ods","xlr","xls","xlsx"]#o,x
-systemRelatedFiles=["bak","cab","cfg","cpl","cur","dll","dmp","drv","icns","ico","ini","ink","msi","sys","tmp"]#b,c,d,i,m.s,y
-videoFiles=["3g2","2gp","avi","flv","h264","m4v","mkv","mov","mp4","mpg","mpeg","rm","swf","vob","wmv"]#2,3,a,f,h,m,r,s,v,w
-wordFiles=["doc","docx","odt","pdf","rtf","tex","txt","wks","wpks","wpd"]#d,o,p,r,t,w
+def printFileName(fileName):
+    try:
+        print(fileName)
+    except:
+        for c in fileName:
+            try:
+                print(c,end="")
+            except:
+                print("?",end="")
 
 def fetchExtension(folderDir):
     #Define all the extension array
@@ -33,20 +26,58 @@ def fetchExtension(folderDir):
     for file in arrayOfFiles:
         if "." in file:
             extension = file.split(".")
-
-            #If it not able to print it doesn't append to the extension list
-            try:
-                print(file,extension[-1])
-                status = True
-            except:
-                pass
-                status = False
-            if status:
-                extensions.append((file,extension[1]))
-
+            extensions.append((file,extension[1]))
     #Returns an array of extensions
     return extensions
 
+def sortCatergory(extensionArray):
+
+    #Database
+    catergorys = [
+    ["audioFiles","aif","cda","mid","midi","mp3","mpa","ogg","wav","wma","wpl"],#a,c,m,o,w
+    ["compressedFiles","7z","arj","deb","pkg","rar","rpm","tar.gz","z","zip"],#7,a,d,p,r,t,z
+    ["discMediaFiles","bin","dmg","iso","toast","vcd"],#b,d,i,t,v
+    ["dataDatabaseFiles","csv","dat","db","dbf","log","mdb","sav","sql","tar","xml"],#c,d,l,m,s,t,x
+    ["executable","apk","bat","bin","cgi","pl","com","exe","gadget","jar","py","wsf"],#a,b,c,p,l,e,g,j,w
+    ["fontFile","fnt","fon","otf","ttf"],#f,f,o,t
+    ["imageFile","ai","bmp","gif","ico","jpeg","jpg","png","ps","psd","svg","tif","tiff"],#a,b,g,i,j,p,s,t
+    ["internetRelatedFiles","asp","aspx","cer","cfm","cgi","pl","css","htm","html","js","jsp","part","php","py","rss","xhtml"],#a,c,p,h,j,p,r,x
+    ["presentationFiles","key","odp","pss","ppt","pptx"],#k,o,p
+    ["programmingFiles","c","class","cpp","cs","h","java","sh","swift","vb"],#c,h,j,s,v
+    ["spreadsheetFiles","ods","xlr","xls","xlsx"],#o,x
+    ["systemRelatedFiles","bak","cab","cfg","cpl","cur","dll","dmp","drv","icns","ico","ini","ink","msi","sys","tmp"],#b,c,d,i,m.s,y
+    ["videoFiles","3g2","2gp","avi","flv","h264","m4v","mkv","mov","mp4","mpg","mpeg","rm","swf","vob","wmv"],#2,3,a,f,h,m,r,s,v,w
+    ["wordFiles","doc","docx","odt","pdf","rtf","tex","txt","wks","wpks","wpd"]#d,o,p,r,t,w
+    ]
+
+    #files and thier catergory
+    fileCatergory =[]
+
+    #extension that are not recognised
+    extensionError=[]
+
+    for file in extensionArray:
+
+        #Makes the code more readable
+        filename = file[0]
+        extension = file[1]
+
+        count = 0
+        for catergory in catergorys:
+            catergoryName = catergory[0]
+            if extension in catergory:
+                fileCatergory.append((filename,catergoryName)) #If the file catergory is found the file name and catergory name is stored in the array
+                #print("found")
+            else:
+                count += 1
+                if count > 13: #After checking through the 13 catergorys its add the extension to list of extension which are not listed
+                    if extension not in extensionError:
+                        extensionError.append(extension)
+
+
+    print("These extension need to be added:\n",extensionError)
+    return fileCatergory #Returns an array of the filename and the catergory
+
 
 fileExtension = fetchExtension(downloadDir)
-print(fileExtension)
+fileCatergory =sortCatergory(fileExtension)
