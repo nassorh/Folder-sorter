055eb7161e9063ee4fe7514323491e6a800358c5
nassorh <nassorh.dev@gmail.com>
2020-04-08 22:09:18 +0100

    GUI added

--- MainOOP.py
+++ MainOOP.py
@@ -1,4 +1,6 @@
 import os, shutil
+import tkinter as tk
+from tkinter import filedialog
 
 class folderSorter():
     def __init__(self,downloadDir):
@@ -100,15 +102,46 @@ class folderSorter():
     def log(self,data):
         file = open("Log.txt","w+")
         for x in data:
-            file.writelines(x)
+            file.writelines(x+"\n")
         file.close()
 
-directory = input("Enter the Directory of the folder Directory: ")
-sort = folderSorter(directory)
-sort.fetchExtension()
-sort.sortCatergory()
-sort.foldersToCreate()
-sort.createFolders()
-sort.moveFiles()
-print("\nFolders sorted please check the log fils for any errors, If any errors were present please send the log file to the admin\nEmail: nassorh.dev@gmail.com")
-input("\nPress any button to exit")
+class GUI():
+    def __init__(self,root):
+        self.master = root
+        self.master.geometry("1000x750")
+        self.master.title("Folder Sorter")
+        self.frame = tk.Frame(self.master)
+        self.frame.grid()
+        self.master.grid_columnconfigure(0, weight=1)
+        #folder_selected = filedialog.askdirectory()
+        #print(folder_selected)
+
+    def mainScreen(self):
+        mainTitle = tk.Label(self.frame, text = "Welcome To The Folder Sorter 2.0")
+        mainTitle.config(font=("Courier","20"))
+        mainTitle.grid(row = 2,column = 3)
+        directoryText = tk.Button(self.frame, text = "Select Folder",command=self.sortAlgorithm)
+        directoryText.config(font=("Courier","10"))
+        directoryText.grid(row=5,column=3)
+
+    def sortAlgorithm(self):
+        folder_selected = filedialog.askdirectory()
+        sort = folderSorter(folder_selected)
+        sort.fetchExtension()
+        sort.sortCatergory()
+        sort.foldersToCreate()
+        sort.createFolders()
+        sort.moveFiles()
+        self.successful()
+
+    def successful(self):
+        top = tk.Toplevel()
+        top.title("Successful")
+        msg = tk.Label(top, text ="Folders sorted please check the log fils for any errors, If any errors were present please send the log file to the admin\nEmail: nassorh.dev@gmail.com")
+        msg.pack()
+        top.mainloop()
+
+root = tk.Tk()
+gui = GUI(root)
+gui.mainScreen()
+root.mainloop()
